e318d7416eb3c515b167a4f813754b29c11e9123
Hitesh Mehta <hteshpooja@gmail.com>
2025-02-16 02:49:33 +0530

    :

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,9 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,9 +11,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
